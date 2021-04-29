9efb603dc8099aa5e24cc8388417483755c51178
MuslicaCrew <360bruno360@gmail.com>
2021-04-29 21:45:58 +0200

    Added modal images

---
+++ modal.js
@@ -0,0 +1,7 @@
+var modalBtn = document.querySelector(".modal-btn");
+
+var modalBg = document.querySelector(".modal-bg");
+
+modalBtn.addEventListener("click", function(){
+    modalBg.classList.add("bg-active");
+});
